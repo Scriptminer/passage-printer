b228381f4a8f14185793e063f1e274d17fcbe4b3
Scriptminer <aidanw@lochan.org>
2024-05-03 15:14:21 +0100

    Initial word document output functionality

--- download_passage.py
+++ download_passage.py
@@ -1,10 +1,35 @@
+from docx import Document
+from docx.enum.style import WD_STYLE_TYPE
 import re
 import requests
 from bs4 import BeautifulSoup
 
 URL = "https://www.bible.com/bible/{version_id}/{book_code}.{chapter}"
 
-response = requests.get(URL.format(version_id="113",book_code="PHP",chapter=2))
+def add_styles(document):
+    note = document.styles.add_style("note", style_type = WD_STYLE_TYPE.CHARACTER)
+    note.font.superscript = True
+
+    label = document.styles.add_style("label", style_type = WD_STYLE_TYPE.CHARACTER)
+    label.font.superscript = True
+
+    small_caps = document.styles.add_style("sc", style_type = WD_STYLE_TYPE.CHARACTER)
+    small_caps.font.small_caps = True
+
+    verse_content = document.styles.add_style("content", style_type = WD_STYLE_TYPE.CHARACTER)
+
+def add_verse_section(paragraph, verse_section_data):
+    verse_section_type = re.findall("^ChapterContent_(.*)__.*$", verse_section_data["class"][0])[0]
+    content = ""
+    if verse_section_type == "note":
+        content = f"[{footnotes.add_note(verse_section_data)}]"
+    else:
+        content = verse_section_data.getText()
+    
+    paragraph.add_run(content, style=verse_section_type)
+
+
+response = requests.get(URL.format(version_id="113",book_code="EXO",chapter=3))
 page = response.content
 
 soup = BeautifulSoup(page)
@@ -28,10 +53,12 @@ formats = {
     "note_label": "^[{content}]^",
 }
 
-markdown_doc = ""
+doc = Document()
+
+add_styles(doc)
 
 chapter_title = soup.find("div", class_=re.compile("ChapterContent_reader")).find("h1").getText()
-markdown_doc += formats["chapter_heading"].format(content=chapter_title)
+doc.add_heading(formats["chapter_heading"].format(content=chapter_title), level=1)
 
 class Footnotes:
     text_notes = []
@@ -56,27 +83,29 @@ class Footnotes:
 
 footnotes = Footnotes()
 
+indents = {
+    # "s1": 0,
+    "p": 0,
+    "q1": 20,
+    "q2": 40,
+}
+
 for chapter_section in chapter.find_all(recursive=False):
     section_type = re.findall("^ChapterContent_(.*)__.*$", chapter_section["class"][0])[0]
     print(f"#{section_type}")
     if section_type == "p" or section_type == "q1" or section_type == "q2":
-        paragraph_text = ""
+        doc_paragraph = doc.add_paragraph()
+        doc_paragraph.paragraph_format.left_indent = indents[section_type]
         for paragraph_section in chapter_section.find_all(recursive=False):
             paragraph_section_type = re.findall("^ChapterContent_(.*)__.*$", paragraph_section["class"][0])[0]
             print(paragraph_section["class"],",",paragraph_section_type)
             if paragraph_section_type == "verse":
                 for verse_section in paragraph_section.find_all(recursive=False):
-                    print(f"...",verse_section)
-                    verse_section_type = re.findall("^ChapterContent_(.*)__.*$", verse_section["class"][0])[0]
-                    if verse_section_type == "note":
-                        paragraph_text += formats["note_label"].format(content = footnotes.add_note(verse_section))
-                    else:
-                        paragraph_text += formats[verse_section_type].format(content = verse_section.getText())
+                    add_verse_section(doc_paragraph, verse_section)
             else:
                 print("UNEXPECTED PART OF SECTION")
-        markdown_doc += formats[section_type].format(content=paragraph_text)
+        # markdown_doc += formats[section_type].format(content=paragraph_text)
 
-markdown_doc += "\n" + footnotes.print_notes()
+doc.add_paragraph(footnotes.print_notes())
 
-with open("out.md", "w") as f:
-    f.write(markdown_doc)
+doc.save("out.docx")
