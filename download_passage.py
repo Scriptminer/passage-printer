4acff175fa110b2a718f7b368ded5c05af77bbd4
Scriptminer <aidanw@lochan.org>
2024-05-28 15:13:53 +0100

    Move add heading and verse functions into Passage class.

--- download_passage.py
+++ download_passage.py
@@ -111,35 +111,6 @@ def resolve_version(version_name):
                 language_info = parts
     return language_info[0]
 
-def add_verse_section(paragraph, verse_section_data, footnotes_handler, passage_pointer):
-    extracted_section_type = re.findall("^ChapterContent_([a-zA-Z0-9]*)_*.*$", verse_section_data["class"][0])
-    verse_section_type = extracted_section_type[0] if len(extracted_section_type) > 0 else verse_section_data["class"][0]
-    content = verse_section_data.getText()
-
-    if verse_section_type == "label":
-        passage_pointer.update_state(content)
-    
-    if passage_pointer.state != PassagePointer.IN_PASSAGE:
-        return
-    
-    if paragraph["paragraph"] == False:
-        add_heading_section(paragraph["doc"], passage_pointer.last_section, passage_pointer) # Add the previous heading
-        paragraph["paragraph"] = paragraph["create"](paragraph["doc"])
-    
-    if verse_section_type == "note":
-        content = f"[{footnotes_handler.add_note(verse_section_data)}]"
-    
-    try:
-        paragraph["paragraph"].add_run(content, style=verse_section_type)
-    except KeyError:
-        print(f"Encountered unexpected verse section type '{verse_section_type}' at v{passage_pointer}. Treating the following as plain text: '{content}'.")
-
-def add_heading_section(doc, heading_text, passage_pointer):
-    if heading_text and passage_pointer.state == PassagePointer.IN_PASSAGE:
-        heading = doc.add_paragraph(style="qa")
-        heading.add_run(heading_text, style="heading")
-    passage_pointer.update_last_section(heading_text)
-
 def add_end_break(doc, break_type=WD_BREAK.COLUMN):
     runs = doc.paragraphs[-1].runs
     if len(runs) == 0:
@@ -284,16 +255,16 @@ class Passage:
                     paragraph_section_type = re.findall("^ChapterContent_([a-zA-Z0-9]*)_*.*$", paragraph_section["class"][0])[0]
                     if paragraph_section_type == "verse":
                         for verse_section in paragraph_section.find_all(recursive=False):
-                            add_verse_section(doc_paragraph, verse_section, self.footnotes_handler, self.passage_pointer)
+                            self.add_verse_section(doc_paragraph, verse_section, self.footnotes_handler, self.passage_pointer)
                             if self.passage_pointer.state == PassagePointer.AFTER_END: break
                     elif paragraph_section_type in ["content", "heading"]:
-                        add_verse_section(doc_paragraph, paragraph_section, self.footnotes_handler, self.passage_pointer)
+                        self.add_verse_section(doc_paragraph, paragraph_section, self.footnotes_handler, self.passage_pointer)
                     else:
                         print(f"Unexpected part of section '{paragraph_section}' found.")
                     if self.passage_pointer.state == PassagePointer.AFTER_END: break
                 if self.passage_pointer.state == PassagePointer.AFTER_END: break
             elif section_type in ["s", "s1"]:
-                add_heading_section(doc, chapter_section.getText(), self.passage_pointer)
+                self.add_heading_section(doc, chapter_section.getText(), self.passage_pointer)
                 
             elif section_type == "b":
                 if self.passage_pointer.IN_PASSAGE:
@@ -308,6 +279,34 @@ class Passage:
         doc.add_paragraph(self.footnotes_handler.print_notes(), style="footnotes")
         doc.add_paragraph(self.copyright_handler.get_copyright_statement(), style="copyright")
 
+    def add_verse_section(self, paragraph, verse_section_data, footnotes_handler, passage_pointer):
+        extracted_section_type = re.findall("^ChapterContent_([a-zA-Z0-9]*)_*.*$", verse_section_data["class"][0])
+        verse_section_type = extracted_section_type[0] if len(extracted_section_type) > 0 else verse_section_data["class"][0]
+        content = verse_section_data.getText()
+
+        if verse_section_type == "label":
+            passage_pointer.update_state(content)
+        
+        if passage_pointer.state != PassagePointer.IN_PASSAGE:
+            return
+        
+        if paragraph["paragraph"] == False:
+            self.add_heading_section(paragraph["doc"], passage_pointer.last_section, passage_pointer) # Add the previous heading
+            paragraph["paragraph"] = paragraph["create"](paragraph["doc"])
+        
+        if verse_section_type == "note":
+            content = f"[{footnotes_handler.add_note(verse_section_data)}]"
+        
+        try:
+            paragraph["paragraph"].add_run(content, style=verse_section_type)
+        except KeyError:
+            print(f"Encountered unexpected verse section type '{verse_section_type}' at v{passage_pointer}. Treating the following as plain text: '{content}'.")
+
+    def add_heading_section(self, paragraph, heading_text, passage_pointer):
+        if heading_text and passage_pointer.state == PassagePointer.IN_PASSAGE:
+            heading = paragraph.add_paragraph(style="qa")
+            heading.add_run(heading_text, style="heading")
+        passage_pointer.update_last_section(heading_text)
 
     def __str__(self):
         end_verse = "end" if self.end_verse == -1 else self.end_verse 
